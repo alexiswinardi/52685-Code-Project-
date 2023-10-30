b933b0c4b40cd1b4190a2d5a13b19f86755a8317
hwinardi <149127909+hwinardi@users.noreply.github.com>
2023-10-30 13:39:54 +1100

    Add files via upload

--- _scripts/Results.js
+++ _scripts/Results.js
@@ -1,12 +1,95 @@
+var _location = 'Chatswood';
+var _cuisines = 'Japanese, Korean';
+var _meal = 'Dinner';
+var _price = '$$';
+var _dataFile = 'https://github.com/alexiswinardi/52685-Code-Project-/blob/bd4c78e64eb9c95b7952f90d0410d17b68e57d80/%5B52685%5D%20Database.xlsx';
+var _json_restaurants;
+
 function formLoad() {
 
     //Get all the items from localStorage created by the GetStarted page
 
-    document.getElementById("location").innerText = localStorage.getItem("_locationInput");
-    document.getElementById("cuisines").innerText = localStorage.getItem("_selectedCuisine");
-    document.getElementById("meal").innerText = localStorage.getItem("_selectedMealType");
-    document.getElementById("price").innerText = localStorage.getItem("_selectedPriceRange");
+    //document.getElementById("location").innerText = localStorage.getItem("_locationInput");
+    //document.getElementById("cuisines").innerText = localStorage.getItem("_selectedCuisine");
+    //document.getElementById("meal").innerText = localStorage.getItem("_selectedMealType");
+    //document.getElementById("price").innerText = localStorage.getItem("_selectedPriceRange");
 
-    //Load data from spreadsheet
-
+    //axios.get(_dataFile)
+    //    .then(function (response) {
+    //        // handle success
+    //        console.log(response);
+    //    })
+    //    .catch(function (error) {
+    //        // handle error
+    //        console.log(error);
+    //    })
+    //    .finally(function () {
+    //        // always executed
+    //        console.log('parse');
+    //    });
 }
+
+function upload() {
+    var files = document.getElementById('file_upload').files;
+    if (files.length == 0) {
+        alert("Please choose any file...");
+        return;
+    }
+    var filename = files[0].name;
+    var extension = filename.substring(filename.lastIndexOf(".")).toUpperCase();
+    if (extension == '.XLS' || extension == '.XLSX') {
+        //Here calling another method to read excel file into json
+        excelToJSON(files[0]);
+    } else {
+        alert("Please select a valid excel file.");
+    }
+}
+
+/*
+    Sample output:
+    
+    0: 
+        Address: "125 Falcon St, Crows Nest NSW 2065"
+        Cuisine: "Japanese"
+        Id: 1
+        Name: "Ryo's Noodles"
+        Phone number: "(02) 9955 0225"
+        Postcode: 2065
+        Website link: "https://www.facebook.com/ryosnoodles/"
+        __rowNum__: 1
+    1:  
+        Address: "346B Illawarra Rd, Marrickville NSW 2204"
+        Cuisine: "Vietnamese"
+        Id: 2
+        Name: "Pho Ha Noi Quan Marrickville"
+        Phone number: "(02) 8018 4928"
+        Postcode: 2204
+        Website link: "https://www.quandoo.com.au/place/pho-ha-noi-quan-32082?aid=63"
+        __rowNum__: 2
+*/
+
+function excelToJSON(file) {
+
+    try {
+        var reader = new FileReader();
+        reader.readAsBinaryString(file);
+        reader.onload = function (e) {
+
+            var data = e.target.result;
+            var workbook = XLSX.read(data, {
+                type: 'binary'
+            });
+            var result = {};
+            var firstSheetName = workbook.SheetNames[1];
+            //reading only first sheet data
+            _json_restaurants = XLSX.utils.sheet_to_json(workbook.Sheets[firstSheetName]);
+            //displaying the json result into HTML table
+            //displayJsonToHtmlTable(jsonData);
+        }
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+
+
